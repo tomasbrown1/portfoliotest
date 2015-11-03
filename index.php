822af3e3e1451a81b50b3e885f5136e84530f4a9
tomasbrown1 <tombrown1@gmail.com>
2015-11-03 17:07:05 +0000

    started horizontally scrolling test site for portfolio

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link type="text/css" rel="stylesheet" href="css/css.css">
+	</head>
+	<body>
+		<?php
+			include ('section.html'); 
+			include ('section.html');
+			include ('section.html');
+		?>
+		<script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+		<script src="js/js.js"></script>
+	</body>
+</html>
