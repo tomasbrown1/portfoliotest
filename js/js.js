6dedd5a8d7d5a901d362a44c756d54bed716d98d
tomasbrown1 <tombrown1@gmail.com>
2015-11-04 14:48:03 +0000

    started creating layou

--- js/js.js
+++ js/js.js
@@ -1,10 +1,9 @@
 $(document).on('keydown wheel', function(e){
-	console.log('hello');
 	if (e.keyCode == 40 || e.originalEvent.wheelDelta < 0) {
 		e.preventDefault();
-		$('html,body').scrollLeft(scrollX + 100);
+		$('html, body').scrollLeft(scrollX + 100);
 	} else if (e.keyCode == 38 || e.originalEvent.wheelDelta > 0) {
 		e.preventDefault();
-		$('html,body').scrollLeft(scrollX - 100);
+		$('html, body').scrollLeft(scrollX - 100);
 	}
 })
